291616631e03bf5b4fd8c9afa3372cec3cb97a31
Ewerton Luna <ewerton.al@gmail.com>
2019-07-22 15:26:55 -0300

    First try finished
    - Added random background color feature
    - Added auto update feature
    - Added a "tags" attribute to the quote object
    - Changed the non existing values in attributes from null to empty strings.

--- js/script.js
+++ js/script.js
@@ -9,44 +9,52 @@ let quotes = [
     {
         quote: "Be yourself; everyone else is already taken.",
         source: 'Oscar Wilde',
-        citation: null,
-        year: null,
+        citation: '',
+        year: '',
+        tags: '#inspiration',
     },
     {
         quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
         source: 'Albert Einstein',
-        citation: null,
-        year: null,
+        citation: '',
+        year: '',
+        tags: '#philosophy',
+
     },
     {
         quote: "No, the opposite of happiness is hopelessness, an endless gray horizon of resignation and indifference. It’s the belief that everything is f****, so why do anything at all",
         source: 'Mark Mason',
         citation: 'Everything Is F****: A Book About Hope',
         year: 2019,
+        tags: '#philosophy',
     },
     {
         quote: "Freedom itself demands discomfort.",
         source: 'Mark Mason',
         citation: 'Everything Is F****: A Book About Hope',
         year: 2019,
+        tags: '#inspiration',
     },
     {
         quote: "I have tried to live my life such that in the hour of my death I would feel joy rather than fear.",
         source: 'Mark Mason',
         citation: 'Everything Is F****: A Book About Hope',
         year: 2019,
+        tags: '#inspiration',
     },
     {
         quote: "In three words I can sum up everything I've learned about life: it goes on.",
         source: 'Robert Frost',
         citation: "This Week Magazine",
         year: 1954,
+        tags: '#philosophy',
     },
     {
         quote: "Don't cry because it's over, smile because it happened.",
         source: 'Doctor Seuss',
-        citation: null,
-        year: null,
+        citation: '',
+        year: '',
+        tags: '#inspiration',
     },
 ];
 
@@ -68,23 +76,49 @@ function print(quoteString){
     output.innerHTML = quoteString;
 }
 
+
+function setRandomBackgroundColor(){
+    // Creating values from 0 to 255 for red, blue and green.
+    let randomColor;
+    let red = Math.random() * 256;
+    let green = Math.random() * 256;
+    let blue = Math.random() * 256;
+
+    // randomColor = 'rgb([red], [green], [bluee])'
+    randomColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+
+    document.body.style.backgroundColor = randomColor;
+}
+
 function printQuote(){
+    setRandomBackgroundColor();
+
     let quote = getRandomQuote();
     let html = '';
-    html += "<p>" + quote.quote + "</p>";
+    html += "<p class='quote'>" + quote.quote + "</p>";
     html += "<p class='source'>" + quote.source;
 
-    if (quote.citation !== null){
+    // If there is a citation, it is added to the html string
+    if (quote.citation !== ''){
         let citationString = "<span class='citation'>" + quote.citation + "</span>";
         html += citationString;
     }
-    if (!quote.year !== null){
+    // If there is a year, it is added to the html string
+    if (!quote.year !== ''){
         let yearString = "<span class='year'>" + quote.year + "</span>";
         html += yearString;
     }
+    let tag = quote.tags;
+
+    // Including tags at the end
+    html += "<span class='tag'>" + quote.tags + "</span>"
     html += '</p>';
+
     print(html); 
 }
 
+setInterval(printQuote, 5000);
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+
